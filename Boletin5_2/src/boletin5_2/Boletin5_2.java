8462382c2fb0696778c0ab2f05d68862e8ad4275
Adry <Adry@Sanisidro>
2016-10-20 12:34:07 +0200

    Acabado

---
+++ Boletin5_2/src/boletin5_2/Boletin5_2.java
@@ -0,0 +1,20 @@
+package boletin5_2;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Adry
+ */
+public class Boletin5_2 {
+   
+    public static void main(String[] args) {
+        short num1=Short.parseShort(JOptionPane.showInputDialog("Numero "));
+        short num2=Short.parseShort(JOptionPane.showInputDialog("Numero "));
+        if (num1>=num2)
+            System.out.println("Resta ="+ (num1-num2)+("\nSuma ="+ (num1+num2)));
+        else
+            System.out.println("Suma ="+ (num1+num2));
+    }
+    
+}
