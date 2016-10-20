cf7b84bc4ebfd784583b01b432adfba3710de936
Adry <Adry@Sanisidro>
2016-10-20 12:26:07 +0200

    Acabado

---
+++ Boletin5_1/src/boletin5_1/Boletin5_1.java
@@ -0,0 +1,17 @@
+package boletin5_1;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Adry
+ */
+public class Boletin5_1 {
+
+       public static void main(String[] args) {
+         int num1=Integer.parseInt(JOptionPane.showInputDialog("Numero "));
+        if(num1>0)
+               System.out.println(num1+" e positivo");
+    }
+    
+}
