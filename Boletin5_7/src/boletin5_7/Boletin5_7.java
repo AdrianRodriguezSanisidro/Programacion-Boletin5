deedb07a74fe7644d3c83209ba367785ed9a9984
Adry <Adry@Sanisidro>
2016-10-27 11:17:47 +0200

    Final 5_7

---
+++ Boletin5_7/src/boletin5_7/Boletin5_7.java
@@ -0,0 +1,14 @@
+package boletin5_7;
+
+/**
+ *
+ * @author Adry
+ */
+public class Boletin5_7 {
+
+  public static void main(String[] args) {
+        Metodos obxArea=new Metodos();
+        obxArea.calAreas();
+    }
+    
+}
